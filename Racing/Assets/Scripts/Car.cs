ae4832f307d26a03d12bf298e32d21b4bf1c7988
Pedro Rangel <42298267+PedroRangelP@users.noreply.github.com>
2022-06-04 02:29:57 -0500

    Moving car with Bezier curves
    Co-Authored-By: Ricardo Juárez <45005831+RichieSjt@users.noreply.github.com>

--- Racing/Assets/Scripts/Car.cs
+++ Racing/Assets/Scripts/Car.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Car : MonoBehaviour
 {
     public GameObject trackPoints;
-    public List<Vector3> points;
+    public List<List<Vector3>> bezierPoints;
     public GameObject theCar;
     public GameObject sph;
     private Vector3[] _vertices;
@@ -14,59 +14,70 @@ public class Car : MonoBehaviour
     private Vector3 _targetPoint;
     private float _param;
     private float _d;
-    private int _pointIdx;
+    private int _curveIdx;
 
     void Start()
     {
         _vertices = theCar.GetComponent<MeshFilter>().mesh.vertices;
+        bezierPoints = new List<List<Vector3>>();
 
-        int children = trackPoints.transform.childCount;
+        Transform bezierCurves = trackPoints.transform;
         
-        for (int i = 0; i < children; ++i)
+        int index = 0;
+        foreach (Transform curve in bezierCurves)
         {
-            Vector3 point = trackPoints.transform.GetChild(i).GetComponent<Transform>().transform.position;
-            points.Add(point);
+            bezierPoints.Add(new List<Vector3>());
+
+            foreach (Transform point in curve) {
+                bezierPoints[index].Add(point.position);
+            }
+            index++;
         }
 
         _position = theCar.transform.position;
-        _startPoint = _position;
-        _pointIdx = 1;
-        _targetPoint = points[_pointIdx];
-
+        _curveIdx = 0;
+        _d = 1000f;
         sph = GameObject.CreatePrimitive(PrimitiveType.Sphere);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // If we reach the target point
+        if(_d < 0.1f)
+        {            
+            // Change the target index
+            if (_curveIdx == bezierPoints.Count-1)
+                _curveIdx = 0;
+            else
+                _curveIdx += 1;
+            
+            _param = 0;
+        }
+        
         sph.transform.position = _position;
 
         _param += 0.001f;
-        _position = Math3D.Interpolation(_startPoint, _targetPoint, _param);
+
+        // Bezier
+        List<Vector3> curve = bezierPoints[_curveIdx];
+
+        _startPoint = curve[0];
+        _targetPoint = curve[curve.Count-1];
+
+        _position = Bezier.EvalBezier(curve, _param);
         _d = Math3D.Magnitude(_targetPoint - _position);
+
+        Vector3 prev = Bezier.EvalBezier(curve, _param-0.0005f);
+        Vector3 dir = _position - prev;
+        Vector3 du = dir.normalized;
+        float angle = Mathf.Rad2Deg * Mathf.Atan(du.z/du.x);
+
+        Debug.DrawLine(_position, dir, Color.red);
+
+        Matrix4x4 r = Transformations.RotateM(angle, Transformations.AXIS.AX_Y);
+        Matrix4x4 t = Transformations.TranslateM(_position.x, _position.y, _position.z - 6.2f);
         
-        // If we reach the target point
-        if(_d < 0.1f)
-        {
-            // Set the new start position
-            _startPoint = points[_pointIdx];
-            
-            // Change the target index
-            if (_pointIdx == points.Count-1)
-                _pointIdx = 0;
-            else
-                _pointIdx += 1;
-            
-            // Change the targert point
-            _targetPoint = points[_pointIdx];
-            _param = 0;
-        }
-
-        // Vector3 prev = Math3D.Interpolation(startPoint, targetPoint, param - 0.00005f);
-        // Vector3 dir = position - prev;
-        // Vector3 du = dir.normalized;
-
-        Matrix4x4 t = Transformations.TranslateM(_position.x, _position.y, _position.z);
-        theCar.GetComponent<MeshFilter>().mesh.vertices = Transformations.ApplyTransformation(_vertices, t);
+        theCar.GetComponent<MeshFilter>().mesh.vertices = Transformations.ApplyTransformation(_vertices, t * r);
     }
 }
