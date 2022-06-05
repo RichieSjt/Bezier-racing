32cd8a180487f09c1a9cf6e9d99426a03922411a
Pedro Rangel <42298267+PedroRangelP@users.noreply.github.com>
2022-06-05 00:11:39 -0500

    Move camera with mouse and keyboard input
    Co-Authored-By: Ricardo Juárez <45005831+RichieSjt@users.noreply.github.com>

--- Racing/Assets/Scripts/CameraFollow.cs
+++ Racing/Assets/Scripts/CameraFollow.cs
@@ -6,16 +6,28 @@ public class CameraFollow : MonoBehaviour
 {
     public GameObject targetCar;
     public float smoothSpeed = 10f;
+	public float sensitivity = 20f;
     public Vector3 offset;
+    private Vector2 rotation = Vector2.zero;
 
     void LateUpdate()
     {
+        // Following the car smoothly
         Vector3 desiredPos = targetCar.GetComponent<Particle>().sphere.transform.position + offset;
         Vector3 smoothPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed * Time.deltaTime);
         transform.position = smoothPos;
+    }
 
-        transform.position = smoothPos;
+    private void Update()
+    {
+        // Rotating camera using mouse
+        rotation.y += Input.GetAxis("Mouse X");
+		rotation.x += -Input.GetAxis("Mouse Y");
+		transform.eulerAngles = (Vector2)rotation * sensitivity;
 
-        transform.LookAt(targetCar.GetComponent<Particle>().sphere.transform.position);
+        // Move the camera with the WASD keys
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(x, 0.0f, z));
     }
 }
